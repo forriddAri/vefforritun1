8081f3dbb2d3c704a64294f45044aebecf510db3
AriGunnar <email@example.com>
2023-11-14 12:20:34 +0000

    myndir og text lesgo

--- verkefni10/src/index.js
+++ verkefni10/src/index.js
@@ -1,57 +1,58 @@
 
-const pokemonCount = 1008;
-var pokedex = {}; // { 1 :  {"name" : "bulbasaur", "img" url, "type" : ["grass", "poison"], "desc" : "...."}  }
+const pokemonCount = 10;
+var pokedex = {}; // {1 : {"name" : "bulbsaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."} }
 
 window.onload = async function() {
-    //getPokemon(1);
+    
     for (let i = 1; i <= pokemonCount; i++) {
         await getPokemon(i);
-        //<div id="ehv tala" class="pokemon-name">NAFN Á POKEMON</div>
+        //<div id="ehv tala" class="pokemon-nafn">NAFN Á POKEMON</div>
         let pokemon = document.createElement("div");
         pokemon.id = i;
         pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
         pokemon.classList.add("pokemon-name");
         pokemon.addEventListener("click", updatePokemon);
         document.getElementById("pokemon-list").append(pokemon);
-
     }
-    
+
+    document.getElementById("pokemon-description").innerText = pokedex[1]["desc"];
+
     console.log(pokedex);
 }
 
 async function getPokemon(num) {
     let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
 
-    let res =  await fetch(url);
+    let res = await fetch(url);
     let pokemon = await res.json();
-    //console.log(pokemon)
+   
 
-    let pokemonName = pokemon["name"]
-    let pokemonType = pokemon["types"]
-    let pokemonImg = pokemon["sprites"]["front-default"]
+    let pokemonName = pokemon["name"];
+    let pokemonType = pokemon["types"];
+    let pokemonImg = pokemon["sprites"]["front_default"];
 
     res = await fetch(pokemon["species"]["url"]);
     let pokemonDesc = await res.json();
 
-    //console.log(pokemonDesc)
-    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor-text"]
+    
+    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"];
 
-    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg,"types" : pokemonType, "desc" : pokemonDesc}
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc};
 
 }
 
 function updatePokemon(){
     document.getElementById("pokemon-img").src = pokedex[this.id]["img"];
 
-    
     //clear types
     let typesDiv = document.getElementById("pokemon-types");
-    while (typesDiv.firstChild){
+    while (typesDiv.firstChild) {
         typesDiv.firstChild.remove();
     }
+
     //update types
     let types = pokedex[this.id]["types"];
-    for (let i = 0; i < types.length; i++){
+    for (let i = 0; i < types.length; i++) {
         let type = document.createElement("span");
         type.innerText = types[i]["type"]["name"].toUpperCase();
         type.classList.add("type-box");
@@ -59,5 +60,6 @@ function updatePokemon(){
         typesDiv.append(type);
     }
 
-
+    //update desc
+    document.getElementById("pokemon-desc").innerText = pokedex[this.id]["desc"];
 }
