9a2c9252ac9e6c7c8cd89d6cb899a61d41f3efdc
Ari Gunnar Kristjónsson <agk19@hi.is>
2023-11-20 20:27:25 +0000

    init fartölvubranch

---
+++ Hópverkefni2/hópverkefni2/src/voruflokkur.js
@@ -0,0 +1,68 @@
+class Flokkur {
+    constructor() {
+      this.apiUrl = 'https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/';
+    }
+  
+    async getProductsByCategory(categoryId) {
+      try {
+        const response = await fetch(`${this.apiUrl}categories`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        return data.items;
+      } catch (error) {
+        console.error('Error fetching products by category:', error);
+        return [];
+      }
+    }
+  
+    displayProductsByCategory(categoryId) {
+      this.getProductsByCategory(categoryId).then(productsArray => {
+        this.renderProducts(productsArray);
+      });
+    }
+  
+    renderProducts(products) {
+      const container = document.getElementById('products-by-category-container');
+      container.innerHTML = ''; 
+      
+      products.forEach(product => {
+        const box = document.createElement('div');
+        box.classList.add('box');
+  
+        const image = document.createElement('img');
+        image.style.maxWidth = '300px';
+        image.style.maxHeight = '150px';
+        image.src = product.image;
+        image.alt = product.title;
+  
+        const title = document.createElement('h3');
+        title.textContent = product.title;
+  
+        const price = document.createElement('p');
+        price.textContent = `Price: $${product.price}`;
+        price.style.display = 'block';
+  
+        box.appendChild(title);
+        box.appendChild(image);
+        box.appendChild(price);
+  
+        container.appendChild(box);
+      });
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const flokkur = new Flokkur();
+  
+    const urlParams = new URLSearchParams(window.location.search);
+    const categoryId = urlParams.get('category');
+  
+    if (categoryId) {
+      flokkur.displayProductsByCategory(categoryId);
+    } else {
+      console.error('Category ID is missing.');
+    }
+  });
