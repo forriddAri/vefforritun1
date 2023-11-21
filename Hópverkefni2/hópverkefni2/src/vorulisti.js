2745412ce2e6408a8dba935158a5d8bc7395c531
asge1r <119693998+asge1r@users.noreply.github.com>
2023-11-21 17:20:26 +0000

    bætt virknir á vörusíðum

--- Hópverkefni2/hópverkefni2/src/vorulisti.js
+++ Hópverkefni2/hópverkefni2/src/vorulisti.js
@@ -1,60 +1,90 @@
 class Vorulisti {
-    async getAllProducts() {
-      try {
-        const response = await fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        return data.items;
-      } catch (error) {
-        console.error('Error fetching products:', error);
-        return [];
+  async getAllProducts() {
+    try {
+      const response = await fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=100');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      const data = await response.json();
+      return data.items;
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return [];
     }
-  
-    displayAllProducts() {
-      this.getAllProducts().then(productsArray => {
-        this.renderProducts(productsArray);
-      });
-    }
+  }
 
-  
-    renderProducts(products) {
-      const container = document.getElementById('all-products-container');
-  
-      products.forEach(product => {
+  displayAllProducts() {
+    this.getAllProducts().then(productsArray => {
+      this.renderProducts(productsArray);
+    });
+  }
+
+  renderProducts(products) {
+    const container = document.getElementById('all-products-container');
+
+    // Group products by category
+    const productsByCategory = {};
+    products.forEach(product => {
+      const category = product.category_title;
+      if (!productsByCategory[category]) {
+        productsByCategory[category] = [];
+      }
+      productsByCategory[category].push(product);
+    });
+
+    // Render products
+    for (const category in productsByCategory) {
+      const productsInCategory = productsByCategory[category];
+
+      // Create a box for the category
+      const categoryBox = document.createElement('div');
+      categoryBox.classList.add('category-box');
+
+      const categoryTitle = document.createElement('h2');
+      categoryTitle.textContent = category;
+
+      categoryBox.appendChild(categoryTitle);
+
+      // Create boxes for three products in the same category
+      const productBoxes = document.createElement('div');
+      productBoxes.classList.add('product-boxes');
+
+      productsInCategory.slice(0, 3).forEach(product => {
         const box = document.createElement('div');
         box.classList.add('box');
-  
+
         const image = document.createElement('img');
-        image.style.maxWidth = '300px';
-        image.style.maxHeight = '150px';
+        image.style.width = '150px'; // Set the desired width
+        image.style.height = '75px'; // Set the desired height
         image.src = product.image;
         image.alt = product.title;
-  
+
         const title = document.createElement('h3');
         title.textContent = product.title;
-  
+
         const price = document.createElement('p');
         price.textContent = `Verð: ${product.price} kr.-`;
         price.style.display = 'block';
-  
-        const category = document.createElement('p');
-        category.textContent = product.category_title;
-        category.style.display = 'block';
-  
+
+        // Add click event to open product details page
+        box.addEventListener('click', () => {
+          window.location.href = `vorusida.html?id=${product.id}`;
+        });
+
         box.appendChild(title);
         box.appendChild(image);
         box.appendChild(price);
-        box.appendChild(category);
-  
-        container.appendChild(box);
+
+        productBoxes.appendChild(box);
       });
+
+      categoryBox.appendChild(productBoxes);
+      container.appendChild(categoryBox);
     }
   }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    const vorulisti = new Vorulisti();
-    vorulisti.displayAllProducts();
-  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const vorulisti = new Vorulisti();
+  vorulisti.displayAllProducts();
+});
--- Hópverkefni2/hópverkefni2/src/vorusida.js
+++ Hópverkefni2/hópverkefni2/src/vorusida.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
   const productId = urlParams.get('id');
 
-  if(productId){
-      getProductDetails(productId);
-  } else{
-      displayErrorMessage("Engin vara fundin");
+  if (productId) {
+    getProductDetails(productId);
+  } else {
+    displayErrorMessage("Engin vara fundin");
   }
 });
 
@@ -13,26 +13,25 @@ function getProductDetails(productId) {
   const apiUrl = 'https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products/';
 
   fetch(apiUrl + productId)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
-      })
-      .then(product => {
-          console.log('Product Details:', product);
-          displayProductDetails(product);
-      })
-      .catch(error => {
-          console.error('Error fetching product details:', error);
-          displayErrorMessage("Error fetching product details");
-      });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(product => {
+      console.log('Product Details:', product);
+      displayProductDetails(product);
+    })
+    .catch(error => {
+      console.error('Error fetching product details:', error);
+      displayErrorMessage("Error fetching product details");
+    });
 }
 
-function displayProductDetails(product){
-  console.log(product);
+function displayProductDetails(product) {
   // Get the container where product details will be displayed
-  const productDetailsContainer = document.getElementById('product-details');
+  const productDetailsContainer = document.getElementById('vorusida');
 
   // Clear previous content
   productDetailsContainer.innerHTML = '';
@@ -50,15 +49,87 @@ function displayProductDetails(product){
   const imageElement = document.createElement('img');
   imageElement.src = product.image;
 
-  // Append elements to the container
-  productDetailsContainer.appendChild(titleElement);
-  productDetailsContainer.appendChild(imageElement);
-  productDetailsContainer.appendChild(priceElement);
-  productDetailsContainer.appendChild(descriptionElement);
+  // Create a parent container for the left and right columns
+  const columnsContainer = document.createElement('div');
+  columnsContainer.style.display = 'flex';
+
+  // Create left and right columns
+  const leftColumn = document.createElement('div');
+  const rightColumn = document.createElement('div');
+
+  // Append elements to the left column
+  leftColumn.appendChild(titleElement);
+  leftColumn.appendChild(descriptionElement);
+
+  // Append elements to the right column
+  rightColumn.appendChild(imageElement);
+  rightColumn.appendChild(priceElement);
+
+  // Append left and right columns to the parent container
+  columnsContainer.appendChild(leftColumn);
+  columnsContainer.appendChild(rightColumn);
+
+  // Append the columns container to the product details container
+  productDetailsContainer.appendChild(columnsContainer);
+
+  // Fetch and display related products
+  getRelatedProducts(product.category_id, product.id, productDetailsContainer);
+}
+
+function getRelatedProducts(categoryId, currentProductId, productDetailsContainer) {
+  const apiUrl = `https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=3&category=${categoryId}&exclude=${currentProductId}`;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(products => {
+      console.log('Related Products:', products);
+      displayRelatedProducts(products, productDetailsContainer);
+    })
+    .catch(error => {
+      console.error('Error fetching related products:', error);
+    });
+}
+
+function displayRelatedProducts(products, productDetailsContainer) {
+  // Create a container for related products
+  const relatedProductsContainer = document.createElement('div');
+  relatedProductsContainer.classList.add('related-products-container');
+
+  // Create a title for related products
+  const relatedProductsTitle = document.createElement('h3');
+  relatedProductsTitle.textContent = 'Tengdar vörur';
+
+  // Append the title to the container
+  relatedProductsContainer.appendChild(relatedProductsTitle);
+
+  // Iterate through related products and display them
+  products.forEach(product => {
+    const relatedProductBox = document.createElement('div');
+    relatedProductBox.classList.add('related-product-box');
+
+    const relatedProductTitle = document.createElement('h4');
+    relatedProductTitle.textContent = product.title;
+
+    const relatedProductImage = document.createElement('img');
+    relatedProductImage.src = product.image;
+
+    relatedProductBox.appendChild(relatedProductTitle);
+    relatedProductBox.appendChild(relatedProductImage);
+
+    relatedProductsContainer.appendChild(relatedProductBox);
+  });
+
+  // Append the related products container after the columns container
+  productDetailsContainer.appendChild(relatedProductsContainer);
 }
 
 function displayErrorMessage(message) {
   // Display an error message on the page
-  const productDetailsContainer = document.getElementById('product-details');
+  const productDetailsContainer = document.getElementById('vorusida');
   productDetailsContainer.innerHTML = `<p>${message}</p>`;
 }
