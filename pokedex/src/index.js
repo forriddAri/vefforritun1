1b93e389706db365d4c8222fa9020b72279ebeda
Ari Gunnar Kristjónsson <agk19@hi.is>
2024-09-04 10:59:44 +0000

    num fix

--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -1,5 +1,5 @@
 
-const pokemonCount = 493;
+const pokemonCount = 151;
 var pokedex = {}; // {1 : {"name" : "bulbsaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."} }
 
 window.onload = async function() {
